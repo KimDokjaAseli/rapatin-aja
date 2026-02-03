a7ec82e3f1b43b911d00416d2e6b6b311ad21dc6
Kim Dokja <memskie324@gmail.com>
2026-02-03 17:33:47 +0700

    push

---
+++ rapatln_backend/config/db.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var DB *mongo.Database
+var Client *mongo.Client
+
+func ConnectDatabase() {
+	// Koneksi ke MongoDB
+	mongoURI := os.Getenv("MONGO_URL")
+	if mongoURI == "" {
+		mongoURI = "mongodb://mongo:smVhjvhpEWPYtnCVKidYzgjIqMUBdcKm@gondola.proxy.rlwy.net:37059"
+	}
+
+	clientOptions := options.Client().ApplyURI(mongoURI)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Could not connect to MongoDB:", err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	DB = client.Database("rapatuy_penjaruy")
+	Client = client
+}
+
+func GetCollection(collectionName string) *mongo.Collection {
+	return DB.Collection(collectionName)
+}
